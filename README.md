3a3200cfed2bd7aeab6b2e472af48d78dc6fff4e
NeonStar-Dev <103542010+NeonStar-Dev@users.noreply.github.com>
2022-04-16 19:16:28 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# SQUIRREL-BOT
+
+Squirrel Bot is the personal assistant of the Treehouse. 
+
+# Functions
+Limited functionality as of now, more to come later. 
+
+### Spanking
+When a message is sent that contains one various words referring to the posterier, Squirrel-Bot will respond with ***SMACK*** and a reaction of an open hand on the message that triggered the spank. 
+
+![Squirrel spanking a user](Resources/README-IMAGES/Spank-Example.PNG "An example of Squirrel performing the spank function")
---
+++ bot.py
@@ -0,0 +1,41 @@
+import os
+
+import discord
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+#print(os.getenv('DISCORD_TOKEN'))
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    #Array of words for the smack response
+    butts = ["butt", "ass", "bend over", "caboose", "tushy", "rump"  ]
+
+    #User requested response when "butt" or a similar word is sent 
+    if any(butt in message.content for butt in butts):
+        response = '***smack*** ' #+ message.author.id 
+        await message.channel.send(response)
+        await message.add_reaction("🖐️")
+    
+
+client.run(TOKEN)
