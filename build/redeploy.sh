0ba3cb46fcf638b3dd2f68ed172933b0c2360089
GitPunk23 <NeonStar.Developer@Outlook.com>
2024-08-03 19:10:53 -0400

    fix redeploy script pt 2

--- build/redeploy.sh
+++ build/redeploy.sh
@@ -1,6 +1,6 @@
 git checkout main
 git pull
-docker build -t squirrel-bot:latest .
+docker build -t squirrel-bot:latest ../
 docker stop squirrel-bot
 docker rm squirrel-bot
 docker run -d -v /opt/squirrel-bot/bot-data:/usr/src/bot/data --name squirrel-bot squirrel-bot:latest
